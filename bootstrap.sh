dff7fd489f945bf1a26402998e2b3993ee035077
Wojtek <wojciech@mindsandmachines.com>
2015-09-22 22:20:36 +0100

    Typo fix.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,9 @@
 
 unamestr=$(uname)
 
-if [[ $unamestr == "Linux"  && -f $(which apt-get) ]]; then
+if [[ $unamestr == "Linux" ]]; then
     sudo pacman-db-upgrade 
     pip || sudo  pacman -S --noconfirm python-pip git
 fi
 
-ansible -h || sudo pacman -S --noconfirmansible
+ansible -h || sudo pacman -S --noconfirm ansible
