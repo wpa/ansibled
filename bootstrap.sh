2f5ea81e43f24d6b03eb9ea2a9c8e250076d0b95
Wojtek Padula <wojciech.padula@gmail.com>
2015-10-20 19:43:32 +0100

    Fix python version.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ unamestr=$(uname)
 if [[ $unamestr == "Linux" ]]; then
     sudo pacman -Sy --noconfirm	
     sudo pacman-db-upgrade 
-    pip || sudo  pacman -S --noconfirm python-pip git
+    pip || sudo  pacman -S --noconfirm python-pip git python2
 fi
 
 ansible -h || sudo pacman -S --noconfirm ansible
